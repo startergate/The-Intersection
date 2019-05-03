b6f64eb7beb854e6b855f91a81783b4ac34073d7
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-05-03 21:41:49 +0900

    Add FlowLayout

--- Game Launcher/GeneratedFiles/ui_GameLauncher.h
+++ Game Launcher/GeneratedFiles/ui_GameLauncher.h
@@ -16,8 +16,62 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QWidget>
+#include <QtWidgets/QStyle>
 
 QT_BEGIN_NAMESPACE
+
+class FlowLayout : public QLayout
+{
+public:
+	explicit FlowLayout(QWidget *parent, int margin = -1, int hSpacing = -1, int vSpacing = -1);
+	explicit FlowLayout(int margin = -1, int hSpacing = -1, int vSpacing = -1);
+	~FlowLayout();
+
+	void addItem(QLayoutItem *item) override;
+	int horizontalSpacing() const;
+	int verticalSpacing() const;
+	Qt::Orientations expandingDirections() const override;
+	bool hasHeightForWidth() const override;
+	int heightForWidth(int) const override;
+	int count() const override;
+	QLayoutItem *itemAt(int index) const override;
+	QSize minimumSize() const override;
+	void setGeometry(const QRect &rect) override;
+	QSize sizeHint() const override;
+	QLayoutItem *takeAt(int index) override;
+
+private:
+	int doLayout(const QRect &rect, bool testOnly) const;
+	int smartSpacing(QStyle::PixelMetric pm) const;
+
+	QList<QLayoutItem *> itemList;
+	int m_hSpace;
+	int m_vSpace;
+};
+
+FlowLayout::FlowLayout(QWidget *parent, int margin, int hSpacing, int vSpacing)
+	: QLayout(parent), m_hSpace(hSpacing), m_vSpace(vSpacing)
+{
+	setContentsMargins(margin, margin, margin, margin);
+}
+
+FlowLayout::FlowLayout(int margin, int hSpacing, int vSpacing)
+	: m_hSpace(hSpacing), m_vSpace(vSpacing)
+{
+	setContentsMargins(margin, margin, margin, margin);
+}
+
+FlowLayout::~FlowLayout()
+{
+	QLayoutItem *item;
+	while ((item = takeAt(0)))
+		delete item;
+}
+
+void FlowLayout::addItem(QLayoutItem *item)
+{
+	itemList.append(item);
+}
 
 class Ui_GameLauncherClass
 {
