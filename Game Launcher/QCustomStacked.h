cf9fddf77e8d8326d6ce517fa1e9b3d2e473b392
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-05-18 23:13:06 +0900

    Fix Error, Add slot  to QCustomStacked

--- Game Launcher/QCustomStacked.h
+++ Game Launcher/QCustomStacked.h
@@ -1,10 +1,18 @@
 #pragma once
 
+#ifndef QCUSTOMSTACKED_H
+#define QCUSTOMSTACKED_H
 #include <QStackedWidget>
 
 class QCustomStacked : public QStackedWidget {
+	Q_OBJECT
+
+public:
 	explicit QCustomStacked(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
 	~QCustomStacked();
 
-
+public slots:
+	void setGameLayer(int gameid);
 };
+
+#endif
