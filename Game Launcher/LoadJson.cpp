705f70740cba841240096b8d570c6249b9ba477c
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-05-30 20:46:03 +0900

    유저데이터 로드 추가

--- Game Launcher/LoadJson.cpp
+++ Game Launcher/LoadJson.cpp
@@ -1,7 +1,23 @@
 #include "LoadJson.h"
 
-rapidjson::Document LoadJson::LoadLibrary() {
+inline rapidjson::Document LoadJson::LoadLibrary() {
 	std::ifstream jsondata("data/game.json");
+
+	rapidjson::IStreamWrapper jsons(jsondata);
+	rapidjson::Document data;
+
+	data.ParseStream(jsons);
+	return data;
+}
+
+inline rapidjson::Document LoadSteam() {
+	rapidjson::Document userinfo = LoadJson::LoadUserData();
+
+
+}
+
+inline rapidjson::Document LoadUserData() {
+	std::ifstream jsondata("data/user.json");
 
 	rapidjson::IStreamWrapper jsons(jsondata);
 	rapidjson::Document data;
--- Game Launcher/LoadJson.h
+++ Game Launcher/LoadJson.h
@@ -1,8 +1,11 @@
 #include <fstream>
 #include <rapidjson/document.h>
 #include <rapidjson/istreamwrapper.h>
+#include <curl>
 
 class LoadJson {
 public:
 	static inline rapidjson::Document LoadLibrary();
+	static inline rapidjson::Document LoadSteam();
+	static inline rapidjson::Document LoadUserData();
 };
