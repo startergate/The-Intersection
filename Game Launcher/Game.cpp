1c819d13a52542a8d56a03c9d96aa51aac61757d
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-15 14:28:06 +0900

    Update Game.cpp

--- Game Launcher/Game.cpp
+++ Game Launcher/Game.cpp
@@ -1,7 +1,7 @@
 #include <Game.h>
 
 Game::Game(int gameid) {
-    this->gameid = gameid; // 게임의 고유 ID를 인수로 받습니다.
+    this->gameid = gameid; // 게임??고유 ID�??�수�?받습?�다.
 }
 
 Game::~Game() {}
@@ -14,9 +14,9 @@ void Game::execute() {
 Json::Value Game::GameListGenerate() {
     Json::Value games_docs;
     LoadJson* lj = new LoadJson;
-    Json::Value games_doc = lj->LoadLibraryW(); // 게임 라이브러리 파일을 로드합니다.
-    Json::Value user = lj->LoadUserData(); // 유저 정보 파일을 로드합니다.
-    Json::Value steamgames = lj->LoadSteam(); // 스팀 라이브러리를 로드합니다.
+    Json::Value games_doc = lj->LoadLibraryW(); // 게임 ?�이브러�??�일??로드?�니??
+    Json::Value user = lj->LoadUserData(); // ?��? ?�보 ?�일??로드?�니??
+    Json::Value steamgames = lj->LoadSteam(); // ?��? ?�이브러리�? 로드?�니??
     Json::Value new_game_doc;
     Json::Value new_games;
 
@@ -24,15 +24,15 @@ Json::Value Game::GameListGenerate() {
     Json::Value& s_games = steamgames["response"]["games"];
     std::list<std::string> toRemove;
 
-    for (Json::ValueIterator it = games.begin(); it != games.end(); it++) if (!(*it)["platform"].asString().compare("steam")) toRemove.push_back((*it)["tiid"].asCString()); // 플랫폼이 스팀인 게임들을 찾습니다.
-    for (auto const& it : toRemove) games_doc["games"].removeMember(it.c_str()); // 플랫폼이 스팀인 게임들을 삭제합니다.
+    for (Json::ValueIterator it = games.begin(); it != games.end(); it++) if (!(*it)["platform"].asString().compare("steam")) toRemove.push_back((*it)["tiid"].asCString()); // ?�랫?�이 ?��???게임?�을 찾습?�다.
+    for (auto const& it : toRemove) games_doc["games"].removeMember(it.c_str()); // ?�랫?�이 ?��???게임?�을 ??��?�니??
 
     for (Json::ValueIterator it = s_games.begin(); it != s_games.end(); it++)
     {
         char buffer[10];
         Json::Value gamedata;
 
-        // 스팀 API로부터 받은 데이터에서 데이터를 추출합니다.
+        // ?��? API로�???받�? ?�이?�에???�이?��? 추출?�니??
         gamedata["tiid"] = itoa((*it)["appid"].asInt(), buffer, 10);
         gamedata["name"] = (*it)["name"].asCString();
         gamedata["isInstalled"] = "false";
@@ -41,31 +41,31 @@ Json::Value Game::GameListGenerate() {
         gamedata["platform"] = "steam";
         gamedata["additional"]["steamid"] = (*it)["appid"].asInt();
 
-        // 추출한 데이터를 게임 라이브러리 객체에 넣습니다.
+        // 추출???�이?��? 게임 ?�이브러�?객체???�습?�다.
         games_doc["games"][itoa((*it)["appid"].asInt(), buffer, 10)] = gamedata;
     }
 
     std::ofstream data;
     data.open("data/game.json");
 
-    data << games_doc; // 게임 라이브러리 객체를 파일로 저장합니다.
+    data << games_doc; // 게임 ?�이브러�?객체�??�일�????�합?�다.
 
-    lj->UploadLibrary(user["sid"]["pid"].asString(), games_doc); // 게임 라이브러리를 서버로 업로드합니다. 백업의 역할을 합니다.
+    lj->UploadLibrary(user["sid"]["pid"].asString(), games_doc); // 게임 ?�이브러리�? ?�버�??�로?�합?�다. 백업????��???�니??
 
-    return games_doc; // 게임 라이브러리를 리턴합니다.
+    return games_doc; // 게임 ?�이브러리�? 리턴?�니??
 }
 
 SteamGame::SteamGame(int gameid, int steamid) : Game(gameid) {
-    this->steamid = steamid; // 스팀 게임에 한해서 steamid 인수를 추가로 받습니다.
+    this->steamid = steamid; // ?��? 게임???�해??steamid ?�수�?추�?�?받습?�다.
 }
 
 SteamGame::~SteamGame() {}
 
 void SteamGame::execute() {
-    // 스팀 실행 명령어를 만듭니다.
+    // ?��? ?�행 명령?��? 만듭?�다.
     std::string executeable = "explorer steam://run/";
     executeable += std::to_string(steamid);
     executeable.append("/");
 
-    system(executeable.c_str()); // 명령어를 실행합니다.
+    system(executeable.c_str()); // 명령?��? ?�행?�니??
 }
